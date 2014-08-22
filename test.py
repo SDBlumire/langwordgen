f188f2c2a1ef186932e885bfaabcf95990abd49b
SDBlumire <sdblumire@gmail.com>
2014-08-22 22:36:15 +0100

    Updated code. Added word

--- test.py
+++ test.py
@@ -39,10 +39,13 @@ def testex(string):
         else:
             return True
         
-def generateswadesh()
+def generateswadesh():
     swadesh = ["I", "you", "we (inclusive)", "this", "that", "who?", "what?", "not", "all", "many", "one", "two", "big", "long", "small", "woman", "man", "person", "fish", "bird", "dog", "louse", "tree", "seed", "leaf", "root", "bark", "skin", "flesh", "blood", "bone", "grease", "egg", "horn", "tail", "feather", "hair", "head", "ear", "eye", "nose", "mouth", "tooth", "tongue", "fingernail", "foot", "knee", "hand", "belly", "neck", "breasts", "heart", "liver", "drink", "eat", "bite", "see", "hear", "know", "sleep", "die", "kill", "swim", "fly", "walk", "come", "lie", "sit", "stand", "give", "say", "sun", "moon", "star", "water", "rain", "stone", "sand", "earth", "cloud", "smoke", "fire", "ash", "burn", "path", "mountain", "red", "green", "yellow", "white", "black", "night", "hot", "cold", "full", "new", "good", "round", "dry", "name", "all", "and", "animal", "ashes", "at", "back", "bad", "bark", "because", "belly", "big", "bird", "bite", "black", "blood", "blood", "blow", "bone", "breast", "breathe", "brother", "burn", "child", "clothing", "cloud", "claw", "cold", "come", "cook", "count", "cut", "dance", "day", "die", "dig", "dirty", "dog", "drink", "dry", "dull", "dust", "ear", "earth", "eat", "egg", "eight", "eye", "fall", "far", "fat/grease", "father", "fear", "feather", "few", "fight", "fire", "fish", "five", "float", "flow", "flower", "fog", "foot", "four", "freeze", "fruit", "full", "give", "good", "grass", "green", "guts", "hair", "hand", "he", "head", "hear", "heart", "heavy", "here", "hit", "hold/take", "horn", "how", "hundred", "hunt", "husband", "I", "ice", "if", "in", "kill", "knee", "know", "lake", "laugh", "leaf", "left (side)", "leg", "lie", "live", "liver", "long", "louse", "man/male", "many", "meat/flesh", "moon", "mother", "mountain", "mouth", "name", "narrow", "near", "neck", "new", "night", "nose", "not", "old", "one", "other", "person", "play", "pull", "push", "rain", "red", "right (correct)", "right (side)", "river", "road", "root", "rope", "rotten", "rub", "salt", "sand", "say", "scratch", "sea", "see", "seed", "seven", "sew", "sharp", "shoot", "short", "sing", "sister", "sit", "skin", "sky", "sleep", "small", "smell", "smoke", "smooth", "snake", "snow", "some", "spear", "spit", "split", "squeeze", "stab/pierce", "stand", "star", "stick", "stone", "straight", "suck", "sun", "swell", "swim", "tail", "ten", "that", "there", "they", "thick", "thin", "think", "this", "three", "throw", "tie", "tongue", "tooth", "tree", "turn", "two", "vomit", "walk", "warm", "wash", "water", "we (exclusive)", "wet", "what?", "when?", "where?", "white", "who?", "wide", "wife", "wind", "wing", "wipe", "with", "woman", "woods", "work", "worm", "yes", "year"]
     for i in range(len(swadesh)):
+        chardef(swadesh[i], getUnusedString)
+
+def getUnusedString():
+    a = genstring()
+    while testex(a) == False:
         a = genstring()
-        while testex(a) == False:
-            a = genstring()
-        chardef(swadesh[i], a)
+    return a
